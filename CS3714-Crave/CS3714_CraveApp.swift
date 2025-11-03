3e616c42f39d19ce179771c85d3456a815f1fb00
Miles1744 <mithomtab@gmail.com>
2025-11-03 16:17:05 -0500

    Initial Commit

---
+++ CS3714-Crave/CS3714_CraveApp.swift
@@ -0,0 +1,17 @@
+//
+//  CS3714_CraveApp.swift
+//  CS3714-Crave
+//
+//  Created by Miles Thomas on 11/3/25.
+//
+
+import SwiftUI
+
+@main
+struct CS3714_CraveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CS3714-Crave/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CS3714-Crave
+//
+//  Created by Miles Thomas on 11/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

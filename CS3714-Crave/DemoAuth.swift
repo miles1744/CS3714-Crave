1bb0b7f75333389bfaac38df215f34b422de0867
Brendan Riordan <brendanriordan123@gmail.com>
2025-11-09 17:02:23 -0500

    firebase

---
+++ CS3714-Crave/DemoAuth.swift
@@ -0,0 +1,8 @@
+//
+//  DemoAuth.swift
+//  CS3714-Crave
+//
+//  Created by Brendan Michael Riordan on 11/9/25.
+//
+
+import Foundation
---
+++ CS3714-Crave/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  CS3714-Crave
+//
+//  Created by Brendan Michael Riordan on 11/9/25.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
